6412c142919d2667cef76a40121de16635cce4bf
Washington Xavier Yépez Yépez <wyepez@outlook.com>
2020-10-07 23:29:38 -0500

    Finishing the core functionality

--- 04-prj-monster-01-starting-setup/app.js
+++ 04-prj-monster-01-starting-setup/app.js
@@ -12,9 +12,15 @@ const app = Vue.createApp({
   },
   computed: {
     monsterBarStyles() {
+      if (this.monsterHealth < 0) {
+        return { width: '0%' };
+      }
       return { width: this.monsterHealth + '%' }
     },
     playerBarStyles() {
+      if (this.playerHealth < 0) {
+        return { width: '0%' };
+      }
       return { width: this.playerHealth + '%' };
     },
     mayUseSpecialAttack() {
@@ -58,6 +64,14 @@ const app = Vue.createApp({
         this.playerHealth += healthValue;
       }
       this.attackPlayer();
+    },
+    startGame() {
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+      this.currentRound = 0;
+    },
+    surrender() {
+      this.playerHealth = 0;
     },
   },
 });
