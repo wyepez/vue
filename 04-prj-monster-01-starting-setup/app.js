e138b4aca3bc6a8705dde2549636341af1893058
Washington Xavier Yépez Yépez <wyepez@outlook.com>
2020-10-13 21:31:39 -0500

    Adding a battle log

--- 04-prj-monster-01-starting-setup/app.js
+++ 04-prj-monster-01-starting-setup/app.js
@@ -8,6 +8,7 @@ const app = Vue.createApp({
       playerHealth: 100,
       monsterHealth: 100,
       currentRound: 0,
+      logMessages: [],
     };
   },
   computed: {
@@ -43,35 +44,47 @@ const app = Vue.createApp({
       this.currentRound++;
       const attackValue = getRandomValue(5, 12);
       this.monsterHealth -= attackValue;
+      this.addLogMessage('player', 'attack', attackValue);
       this.attackPlayer();
     },
     attackPlayer() {
       const attackValue = getRandomValue(8, 15);
       this.playerHealth -= attackValue;
+      this.addLogMessage('monster', 'attack', attackValue);
     },
     specialAttackMonster() {
       this.currentRound++;
       const attackValue = getRandomValue(10, 25);
       this.monsterHealth -= attackValue;
+      this.addLogMessage('player', 'attack', attackValue);
       this.attackPlayer();
     },
     healPlayer() {
       this.currentRound++;
-      const healthValue = getRandomValue(8, 20);
-      if (this.playerHealth + healthValue > 100) {
+      const healValue = getRandomValue(8, 20);
+      if (this.playerHealth + healValue > 100) {
         this.playerHealth = 100;
       } else {
-        this.playerHealth += healthValue;
+        this.playerHealth += healValue;
       }
+      this.addLogMessage('player', 'heal', healValue);
       this.attackPlayer();
     },
     startGame() {
       this.playerHealth = 100;
       this.monsterHealth = 100;
       this.currentRound = 0;
+      this.logMessages = [];
     },
     surrender() {
       this.playerHealth = 0;
+    },
+    addLogMessage(who, what, value) {
+      this.logMessages.unshift({
+        actionBy: who,
+        actionType: what,
+        actionValue: value,
+      });
     },
   },
 });
